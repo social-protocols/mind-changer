834e719ff0e7928ec4bdad0aba3954a91c39d98e
Felix Dietze <github@felx.me>
2024-09-13 14:19:48 +0200

    clean up main

--- src/main.rs
+++ src/main.rs
@@ -73,26 +73,16 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn process_item(item_id: &str, conn: &Connection) -> Result<(), Box<dyn Error>> {
-    let score = calculate_change_of_mind(item_id, &conn)?;
-    println!("item: {}, change-of-mind: {}", item_id, score);
-    if score != -1.0 {
-        conn.execute(
-            "insert or replace into scores (noteId, change) values (?1, ?2)",
-            (item_id, score),
-        )?;
-    }
-    Ok(())
-}
-
-fn calculate_change_of_mind(item_id: &str, conn: &Connection) -> Result<f64, Box<dyn Error>> {
+    let item_id = item_id;
+    let conn: &Connection = &conn;
     let context_size = CONTEXT_SIZE;
     let mut stmt_voters_on_note =
         conn.prepare("select raterParticipantId, createdAtMillis from ratings where noteId = ?1")?;
     // TODO: filter users/items with only one vote
     let mut stmt_votes_before =
-        conn.prepare("select noteId, helpfulnessLevel from ratings where raterParticipantId = ?1 and createdAtMillis < ?2 and helpfulnesslevel != '' order by createdAtMillis desc limit ?3;")?;
+            conn.prepare("select noteId, helpfulnessLevel from ratings where raterParticipantId = ?1 and createdAtMillis < ?2 and helpfulnesslevel != '' order by createdAtMillis desc limit ?3;")?;
     let mut stmt_votes_after =
-        conn.prepare("select noteId, helpfulnessLevel from ratings where raterParticipantId = ?1 and createdAtMillis > ?2 and helpfulnesslevel != '' order by createdAtMillis asc limit ?3;")?;
+            conn.prepare("select noteId, helpfulnessLevel from ratings where raterParticipantId = ?1 and createdAtMillis > ?2 and helpfulnesslevel != '' order by createdAtMillis asc limit ?3;")?;
 
     let user_iter = stmt_voters_on_note.query_map(params![item_id], |row| {
         Ok(User {
@@ -179,35 +169,47 @@ fn calculate_change_of_mind(item_id: &str, conn: &Connection) -> Result<f64, Box
 
     if item_ids.len() < 3 || user_ids.len() < 3 || votes_before.len() == 0 || votes_after.len() == 0
     {
-        return Ok(-1.0);
+        println!("skipping");
+        return Ok(());
     }
 
     let observed_matrix_before = compute_observed_matrix(&votes_before, &item_ids, &user_ids);
     let observed_matrix_after = compute_observed_matrix(&votes_after, &item_ids, &user_ids);
 
     // just for initializing the guess.
-    let mental_model_after_tmp = compute_mental_model(
-        &observed_matrix_after,
-        COMPLETION_RANK,
-        COMPLETION_TOLERANCE,
-        COMPLETION_MAX_ITERATIONS,
-        None,
-    );
-    let mental_model_before = compute_mental_model(
-        &observed_matrix_before,
-        COMPLETION_RANK,
-        COMPLETION_TOLERANCE,
-        COMPLETION_MAX_ITERATIONS,
-        Some(&mental_model_after_tmp),
-    );
+    let mental_model_after_tmp = {
+        let observed_matrix = &observed_matrix_after;
+        matrix_completion_svd(
+            observed_matrix.clone(),
+            COMPLETION_RANK,
+            COMPLETION_TOLERANCE,
+            COMPLETION_MAX_ITERATIONS,
+            None,
+        )
+    };
+    let mental_model_before = {
+        let observed_matrix = &observed_matrix_before;
+        let initial_guess = Some(&mental_model_after_tmp);
+        matrix_completion_svd(
+            observed_matrix.clone(),
+            COMPLETION_RANK,
+            COMPLETION_TOLERANCE,
+            COMPLETION_MAX_ITERATIONS,
+            initial_guess.cloned(),
+        )
+    };
 
-    let mental_model_after = compute_mental_model(
-        &observed_matrix_after,
-        COMPLETION_RANK,
-        COMPLETION_TOLERANCE,
-        COMPLETION_MAX_ITERATIONS,
-        Some(&mental_model_before),
-    );
+    let mental_model_after = {
+        let observed_matrix = &observed_matrix_after;
+        let initial_guess = Some(&mental_model_before);
+        matrix_completion_svd(
+            observed_matrix.clone(),
+            COMPLETION_RANK,
+            COMPLETION_TOLERANCE,
+            COMPLETION_MAX_ITERATIONS,
+            initial_guess.cloned(),
+        )
+    };
 
     let top = 5.min(item_ids.len());
     println!("Observed matrix before voting:");
@@ -227,10 +229,18 @@ fn calculate_change_of_mind(item_id: &str, conn: &Connection) -> Result<f64, Box
             .to_owned(),
     );
 
-    let rmse = root_mean_square_error(&mental_model_before, &mental_model_after) / 2.0; // range is from 0 to 2
-    println!("mind change: {}", rmse);
+    let rmse = root_mean_square_error(&mental_model_before, &mental_model_after); // range is from 0 to 2
 
-    Ok(rmse)
+    let mind_change = rmse / 2.0;
+    println!("mind change: {}", mind_change);
+    println!("item: {}, change-of-mind: {}", item_id, mind_change);
+    if mind_change != -1.0 {
+        conn.execute(
+            "insert or replace into scores (noteId, change) values (?1, ?2)",
+            (item_id, mind_change),
+        )?;
+    }
+    Ok(())
 }
 
 fn root_mean_square_error(matrix1: &Array2<f64>, matrix2: &Array2<f64>) -> f64 {
@@ -255,20 +265,4 @@ fn compute_observed_matrix(
     }
 
     observed_matrix
-}
-
-fn compute_mental_model(
-    observed_matrix: &Array2<f64>,
-    completion_rank: usize,
-    completion_tolerance: f64,
-    completion_max_iterations: usize,
-    initial_guess: Option<&Array2<f64>>,
-) -> Array2<f64> {
-    matrix_completion_svd(
-        observed_matrix.clone(),
-        completion_rank,
-        completion_tolerance,
-        completion_max_iterations,
-        initial_guess.cloned(),
-    )
 }
